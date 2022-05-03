55db8a3898cc6bd6cccccc8fb0601ed377a13d31
Muroz <dazc35@mun.ca>
2022-05-02 21:22:13 -0300

    Added husky pre-commit hooks

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 const port = 3000;
+
+app.use(cors());
+app.use(helmet());
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
