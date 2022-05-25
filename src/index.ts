f76138f1683ce68c260860e9156eacd35568f8eb
Muroz <dazc35@mun.ca>
2022-05-25 13:33:23 -0300

    Added health check and fallback endpoints

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,20 @@ const port = 3000;
 app.use(cors());
 app.use(helmet());
 
+app.get('/health', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date(),
+  };
+
+  res.status(200).json(data);
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json('Not found');
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
